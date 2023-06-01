6fc0e4acba6d2ffe0eb4ab7638ef7eb6be7bf6c7
Silvano <silva_oliv@hotmail.com>
2023-06-01 12:22:35 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace SimpleDataInserterIntoWordDocuments
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChooseFiles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnChooseFiles
+            // 
+            this.btnChooseFiles.Location = new System.Drawing.Point(12, 12);
+            this.btnChooseFiles.Name = "btnChooseFiles";
+            this.btnChooseFiles.Size = new System.Drawing.Size(149, 23);
+            this.btnChooseFiles.TabIndex = 0;
+            this.btnChooseFiles.Text = "button1";
+            this.btnChooseFiles.UseVisualStyleBackColor = true;
+            this.btnChooseFiles.Click += new System.EventHandler(this.btnChooseFiles_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnChooseFiles);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnChooseFiles;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+using System.Windows.Forms;
+using Spire.Doc;
+using System;
+
+
+namespace SimpleDataInserterIntoWordDocuments
+{
+    public partial class Form1 : Form
+    {
+        public List<string> docPaths = new List<string>();
+        public List<string> campos = new List<string>();
+
+
+        public string pathsTest = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Simple Data Inserter Into Word Documents";
+            btnChooseFiles.Text = "Selecionar documentos";
+        }
+
+        private void btnChooseFiles_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog chooseDocs = new OpenFileDialog();
+            chooseDocs.Title = "Selecionar documentos a tratar";
+
+            //Filtro de tipos de ficheiros a manipular
+            chooseDocs.Filter = "Microsoft Word Templates (*.dotx)|*.dotx|Microsoft Word Documents (*.docx)|*.docx";
+
+            //Ativa a seleção de vários ficheiros
+            chooseDocs.Multiselect = true;
+            
+            //Guarda o ultimo caminho usado (para melhor usabilidade)
+            chooseDocs.RestoreDirectory = true;
+
+            //Executa a caixa de diálogo
+            //Se houver submissão executa ação
+            if (chooseDocs.ShowDialog() == DialogResult.OK)
+            {
+                //Captura o caminho dos ficheiros para vir a manipular posteriormente
+                foreach (string paths in chooseDocs.FileNames)
+                {
+                    docPaths.Add(paths);
+                    pathsTest += paths + "\n";
+                }
+                MessageBox.Show(pathsTest);
+                pathsTest = "";
+
+
+
+                //Instanciação de váriavel para manipular um documento Word
+                Document document = new Document();
+
+                //Carrega o documento
+                document.LoadFromFile(chooseDocs.FileNames[0]);
+
+                //Percorrer todos os campos presentes no documento
+                //Adicioná-los a uma lista evitando repetições
+                foreach (var item in document.MailMerge.GetMergeFieldNames())
+                {
+                    if (!campos.Contains(item))
+                    {
+                        campos.Add(item);
+                    }
+                }
+            }
+
+
+            foreach (string campo in campos)
+            {
+                pathsTest += campo + "\n";
+            }
+            MessageBox.Show(pathsTest);
+            
+
+
+        }
+    }
+}
